a30a0b74813c735405622771373cd6d7b472ebb9
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-11-30 23:52:53 -0300

    Cria e mapeia entidades Cozinha e Restaurante

---
+++ src/main/java/com/danielarrais/algafood/domain/model/Cozinha.java
@@ -0,0 +1,20 @@
+package com.danielarrais.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String nome;
+}
---
+++ src/main/java/com/danielarrais/algafood/domain/model/Restaurante.java
@@ -0,0 +1,20 @@
+package com.danielarrais.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String nome;
+}
