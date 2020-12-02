3b0c4f7977c400d4d63f0ba453f6148c27db71a8
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-02 00:48:32 -0300

    Testa método de exclusão de cozinha

--- src/main/java/com/danielarrais/algafood/AlgafoodMainApiApplication.java
+++ src/main/java/com/danielarrais/algafood/AlgafoodMainApiApplication.java
@@ -20,21 +20,42 @@ public class AlgafoodMainApiApplication {
                 .nome("President Jackin")
                 .build();
 
+        Cozinha cozinha2 =  Cozinha
+                .builder()
+                .nome("President Jackin2")
+                .build();
+
         Cozinha novaCozinha = cadastroCozinha.salvar(cozinha);
+        Cozinha novaCozinha2 = cadastroCozinha.salvar(cozinha2);
+
         Long idNovaCozinha = novaCozinha.getId();
 
         cadastroCozinha.listar().stream().forEach((entity) -> {
             System.out.println(entity.getNome());
         });
 
+        System.out.println("=================================================");
+
         Cozinha cozinhaRecemInserida = cadastroCozinha.buscarPorId(idNovaCozinha);
 
-        System.out.println("Cozinha Recém Inserida: " + cozinhaRecemInserida.getNome());
+        System.out.println("Cozinha Inserida: " + cozinhaRecemInserida.getNome());
+
+        System.out.println("=================================================");
 
         cozinhaRecemInserida.setNome("President Jackin 2");
 
         Cozinha cozinhaAtualizada = cadastroCozinha.salvar(cozinhaRecemInserida);
 
         System.out.println("Cozinha Atualizada: " + cozinhaAtualizada.getNome());
+
+        System.out.println("=================================================");
+
+        cadastroCozinha.remover(cozinhaAtualizada);
+
+        System.out.println("=================================================");
+
+        cadastroCozinha.listar().stream().forEach((entity) -> {
+            System.out.println(entity.getNome());
+        });
     }
 }
