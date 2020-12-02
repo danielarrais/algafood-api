c5be915eefddabf72e00b8d4a6b5f94a6dad9beb
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-02 00:11:22 -0300

    Cria classe para testar consultas na tabela cozinha usando com JPA

---
+++ src/main/java/com/danielarrais/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,19 @@
+package com.danielarrais.algafood.jpa;
+
+import com.danielarrais.algafood.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Cozinha> listar() {
+        return entityManager.createQuery("select c from Cozinha as c", Cozinha.class).getResultList();
+    }
+}
