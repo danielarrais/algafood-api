6ee55e7f9d2cd65bb1cfb24516f7ef57ab51bf16
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-03 01:38:15 -0300

    Testa relação de restaurante com cozinha

--- src/main/java/com/danielarrais/algafood/AlgafoodMainApiApplication.java
+++ src/main/java/com/danielarrais/algafood/AlgafoodMainApiApplication.java
@@ -1,7 +1,9 @@
 package com.danielarrais.algafood;
 
 import com.danielarrais.algafood.domain.model.Cozinha;
+import com.danielarrais.algafood.domain.model.Restaurante;
 import com.danielarrais.algafood.domain.repository.CozinhaRepository;
+import com.danielarrais.algafood.domain.repository.RestauranteRepository;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,45 +16,43 @@ public class AlgafoodMainApiApplication {
                 .run(args);
 
         CozinhaRepository cozinhas = applicationContext.getBean(CozinhaRepository.class);
+        RestauranteRepository restaurantes = applicationContext.getBean(RestauranteRepository.class);
 
-        Cozinha cozinha =  Cozinha
+        Cozinha cozinhaFrancesa =  Cozinha
+                .builder()
+                .nome("Francesa")
+                .build();
+
+        cozinhaFrancesa = cozinhas.add(cozinhaFrancesa);
+
+        Restaurante restauranteJackin =  Restaurante
                 .builder()
                 .nome("President Jackin")
+                .cozinha(cozinhaFrancesa)
                 .build();
 
-        Cozinha cozinha2 =  Cozinha
-                .builder()
-                .nome("President Jackin2")
-                .build();
-
-        Cozinha novaCozinha = cozinhas.add(cozinha);
-        Cozinha novaCozinha2 = cozinhas.add(cozinha2);
-
-        Long idNovaCozinha = novaCozinha.getId();
+        restauranteJackin = restaurantes.add(restauranteJackin);
 
         cozinhas.all().stream().forEach((entity) -> {
             System.out.println(entity.getNome());
         });
 
+        System.out.println("===========================================================");
+
+        System.out.println("Restaurante Inserid: " + cozinhaFrancesa.getNome());
+        System.out.println("Cozinha do Restaurante Inserido: " + restauranteJackin.getCozinha().getNome());
+
+        System.out.println("================= MUDANDO NOME DO RESTAURANTE =============");
+
+        restauranteJackin.setNome("President Jackin - II");
+        restauranteJackin = restaurantes.add(restauranteJackin);
+        System.out.println("Novo nome do restaurante: " + restauranteJackin.getNome());
+
+        System.out.println("================== REMOVENDO RESTAURANTE ==================");
+
+        restaurantes.remove(restauranteJackin);
+        cozinhas.remove(cozinhaFrancesa);
+
         System.out.println("=================================================");
-
-        Cozinha cozinhaRecemInserida = cozinhas.find(idNovaCozinha);
-        System.out.println("Cozinha Inserida: " + cozinhaRecemInserida.getNome());
-
-        System.out.println("=================================================");
-
-        cozinhaRecemInserida.setNome("President Jackin 2");
-        Cozinha cozinhaAtualizada = cozinhas.add(cozinhaRecemInserida);
-        System.out.println("Cozinha Atualizada: " + cozinhaAtualizada.getNome());
-
-        System.out.println("=================================================");
-
-        cozinhas.remove(cozinhaAtualizada);
-
-        System.out.println("=================================================");
-
-        cozinhas.all().stream().forEach((entity) -> {
-            System.out.println(entity.getNome());
-        });
     }
 }
