519ba4e59f3d5083d3ed5316b7afcb6f063c893b
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-03 00:42:33 -0300

    Chama novo repository em vez da classe antiga CadastroCozinha

--- src/main/java/com/danielarrais/algafood/AlgafoodMainApiApplication.java
+++ src/main/java/com/danielarrais/algafood/AlgafoodMainApiApplication.java
@@ -1,7 +1,7 @@
 package com.danielarrais.algafood;
 
 import com.danielarrais.algafood.domain.model.Cozinha;
-import com.danielarrais.algafood.jpa.CadastroCozinha;
+import com.danielarrais.algafood.domain.repository.CozinhaRepository;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,7 +13,7 @@ public class AlgafoodMainApiApplication {
                 .web(WebApplicationType.NONE)
                 .run(args);
 
-        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        CozinhaRepository cozinhaRepository = applicationContext.getBean(CozinhaRepository.class);
 
         Cozinha cozinha =  Cozinha
                 .builder()
@@ -25,36 +25,33 @@ public class AlgafoodMainApiApplication {
                 .nome("President Jackin2")
                 .build();
 
-        Cozinha novaCozinha = cadastroCozinha.salvar(cozinha);
-        Cozinha novaCozinha2 = cadastroCozinha.salvar(cozinha2);
+        Cozinha novaCozinha = cozinhaRepository.salvar(cozinha);
+        Cozinha novaCozinha2 = cozinhaRepository.salvar(cozinha2);
 
         Long idNovaCozinha = novaCozinha.getId();
 
-        cadastroCozinha.listar().stream().forEach((entity) -> {
+        cozinhaRepository.listar().stream().forEach((entity) -> {
             System.out.println(entity.getNome());
         });
 
         System.out.println("=================================================");
 
-        Cozinha cozinhaRecemInserida = cadastroCozinha.buscarPorId(idNovaCozinha);
-
+        Cozinha cozinhaRecemInserida = cozinhaRepository.buscarPorId(idNovaCozinha);
         System.out.println("Cozinha Inserida: " + cozinhaRecemInserida.getNome());
 
         System.out.println("=================================================");
 
         cozinhaRecemInserida.setNome("President Jackin 2");
-
-        Cozinha cozinhaAtualizada = cadastroCozinha.salvar(cozinhaRecemInserida);
-
+        Cozinha cozinhaAtualizada = cozinhaRepository.salvar(cozinhaRecemInserida);
         System.out.println("Cozinha Atualizada: " + cozinhaAtualizada.getNome());
 
         System.out.println("=================================================");
 
-        cadastroCozinha.remover(cozinhaAtualizada);
+        cozinhaRepository.remover(cozinhaAtualizada);
 
         System.out.println("=================================================");
 
-        cadastroCozinha.listar().stream().forEach((entity) -> {
+        cozinhaRepository.listar().stream().forEach((entity) -> {
             System.out.println(entity.getNome());
         });
     }
