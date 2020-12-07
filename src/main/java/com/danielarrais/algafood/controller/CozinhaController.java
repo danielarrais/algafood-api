4ba0d7e2c2d441b3e8a93cef0899952bb6edf9cd
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-07 01:34:44 -0300

    Cria endpoint de cadastro de Cozinha

--- src/main/java/com/danielarrais/algafood/controller/CozinhaController.java
+++ src/main/java/com/danielarrais/algafood/controller/CozinhaController.java
@@ -4,12 +4,11 @@ import com.danielarrais.algafood.domain.model.Cozinha;
 import com.danielarrais.algafood.domain.repository.CozinhaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -29,6 +28,13 @@ public class CozinhaController {
     public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
         Cozinha cozinha = cozinhaRepository.find(id);
 
-        return ResponseEntity.ok(cozinha);
+        return Optional.ofNullable(cozinha)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public void adicionar(@RequestBody Cozinha cozinha) {
+        cozinhaRepository.add(cozinha);
     }
 }
