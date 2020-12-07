b667f2f52b0a7ae7c5430bed9817b78239d32189
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-06 23:37:58 -0300

    Cria endpoint de busca de cozinha por ID

--- src/main/java/com/danielarrais/algafood/controller/CozinhaController.java
+++ src/main/java/com/danielarrais/algafood/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.danielarrais.algafood.controller;
 import com.danielarrais.algafood.domain.model.Cozinha;
 import com.danielarrais.algafood.domain.repository.CozinhaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class CozinhaController {
     @GetMapping()
     public List<Cozinha> listar() {
         return cozinhaRepository.all();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id) {
+        return cozinhaRepository.find(id);
     }
 }
