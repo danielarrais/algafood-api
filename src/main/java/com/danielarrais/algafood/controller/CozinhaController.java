471499c728c47cd7ce0a3083ea3d71e6d5454fd1
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-06 22:42:31 -0300

    Cria controller REST com endpoint de listagem de cozinhas

---
+++ src/main/java/com/danielarrais/algafood/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.danielarrais.algafood.controller;
+
+import com.danielarrais.algafood.domain.model.Cozinha;
+import com.danielarrais.algafood.domain.repository.CozinhaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+    private final CozinhaRepository cozinhaRepository;
+
+    public CozinhaController(CozinhaRepository cozinhaRepository) {
+        this.cozinhaRepository = cozinhaRepository;
+    }
+
+    @GetMapping()
+    public List<Cozinha> listar() {
+        return cozinhaRepository.all();
+    }
+}
