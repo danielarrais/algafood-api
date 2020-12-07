64f36e9af42bfaec897512badf994de62587eab3
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-07 02:42:05 -0300

    Atualiza template de rest controller com novos endpoints básicos

--- .idea/fileTemplates/Basic Rest Controller.java
+++ .idea/fileTemplates/Basic Rest Controller.java
@@ -1,8 +1,8 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #set($MODEL_NAME_CAMEL_CASE = $MODEL_NAME.substring(0, 1).toLowerCase() + $MODEL_NAME.substring(1))
 
-import com.danielarrais.algafood.domain.model.Cozinha;
-import com.danielarrais.algafood.domain.repository.CozinhaRepository;
+import com.danielarrais.algafood.domain.model.${MODEL_NAME};
+import com.danielarrais.algafood.domain.repository.${MODEL_NAME}Repository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +23,12 @@ public class ${MODEL_NAME}Controller {
 
     @GetMapping()
     public List<${MODEL_NAME}> listar() {
-        return ${MODEL_NAME_CAMEL_CASE}Repository.all();
+        return ${MODEL_NAME_CAMEL_CASE}Repository.listar();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<${MODEL_NAME}> buscar(@PathVariable Long id) {
-        ${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE} = ${MODEL_NAME_CAMEL_CASE}Repository.find(id);
+        ${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE} = ${MODEL_NAME_CAMEL_CASE}Repository.buscar(id);
 
         return Optional
                 .ofNullable(${MODEL_NAME_CAMEL_CASE})
@@ -39,19 +39,31 @@ public class ${MODEL_NAME}Controller {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void adicionar(@RequestBody ${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}) {
-        ${MODEL_NAME_CAMEL_CASE}Repository.add(${MODEL_NAME_CAMEL_CASE});
+        ${MODEL_NAME_CAMEL_CASE}Repository.adicionar(${MODEL_NAME_CAMEL_CASE});
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody ${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}) {
         return Optional
-                .ofNullable(${MODEL_NAME_CAMEL_CASE}Repository.find(id))
+                .ofNullable(${MODEL_NAME_CAMEL_CASE}Repository.buscar(id))
                 .map(${MODEL_NAME_CAMEL_CASE}Atual -> {
                     BeanUtils.copyProperties(${MODEL_NAME_CAMEL_CASE}, ${MODEL_NAME_CAMEL_CASE}Atual, "id");
-                    ${MODEL_NAME_CAMEL_CASE}Repository.add(${MODEL_NAME_CAMEL_CASE}Atual);
+                    ${MODEL_NAME_CAMEL_CASE}Repository.adicionar(${MODEL_NAME_CAMEL_CASE}Atual);
 
                     return ResponseEntity.ok((Void) null);
                 }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        return Optional
+                .ofNullable(${MODEL_NAME_CAMEL_CASE}Repository.buscar(id))
+                .map(${MODEL_NAME_CAMEL_CASE} -> {
+                    ${MODEL_NAME_CAMEL_CASE}Repository.remover(${MODEL_NAME_CAMEL_CASE});
+
+                    return ResponseEntity.status(HttpStatus.NO_CONTENT).body((Void) null);
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
 
