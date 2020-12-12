4eec9ca3d060e4ba4521de721289ec52dd9944bf
Daniel Arrais <daniel.carvalho@a.catolica-to.edu.br>
2020-12-12 16:44:37 -0300

    Trata não existência de uma cozinha

--- src/main/java/com/danielarrais/algafood/domain/service/RestauranteService.java
+++ src/main/java/com/danielarrais/algafood/domain/service/RestauranteService.java
@@ -1,8 +1,12 @@
 package com.danielarrais.algafood.domain.service;
 
 import com.danielarrais.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.danielarrais.algafood.domain.model.Cozinha;
 import com.danielarrais.algafood.domain.model.Restaurante;
+import com.danielarrais.algafood.domain.repository.CozinhaRepository;
 import com.danielarrais.algafood.domain.repository.RestauranteRepository;
+import com.danielarrais.algafood.util.ExceptionUtils;
+import lombok.SneakyThrows;
 import org.springframework.beans.BeanUtils;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -13,9 +17,11 @@ import java.util.Objects;
 @Service
 public class RestauranteService {
     private final RestauranteRepository restauranteRepository;
+    private final CozinhaRepository cozinhaRepository;
 
-    public RestauranteService(RestauranteRepository restauranteRepository) {
+    public RestauranteService(RestauranteRepository restauranteRepository, CozinhaRepository cozinhaRepository) {
         this.restauranteRepository = restauranteRepository;
+        this.cozinhaRepository = cozinhaRepository;
     }
 
     public List<Restaurante> listar() {
@@ -26,12 +32,14 @@ public class RestauranteService {
         return restauranteRepository.buscar(restauranteId);
     }
 
+    @SneakyThrows
     public void salvar(Restaurante restaurante) {
-        try {
-            restauranteRepository.salvar(restaurante);
-        } catch (EmptyResultDataAccessException exception) {
-            throw new EntidadeNaoEncontradaException(restaurante.getId());
-        }
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+        ExceptionUtils.throwIsNull(cozinha, new EntidadeNaoEncontradaException("Cozinha", cozinhaId));
+
+        restauranteRepository.salvar(restaurante);
     }
 
     public void atualizar(Long id, Restaurante restaurante) {
