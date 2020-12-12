4b366bad1b161e82755ba494a6b6d2dc9fc50e21
Daniel Arrais <daniel.carvalho@a.catolica-to.edu.br>
2020-12-12 15:25:09 -0300

    Cria método exclusivo para atualizar entidade

--- src/main/java/com/danielarrais/algafood/domain/service/CozinhaService.java
+++ src/main/java/com/danielarrais/algafood/domain/service/CozinhaService.java
@@ -4,11 +4,13 @@ import com.danielarrais.algafood.domain.exception.EntidadeEmUsoException;
 import com.danielarrais.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.danielarrais.algafood.domain.model.Cozinha;
 import com.danielarrais.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CozinhaService {
@@ -27,14 +29,30 @@ public class CozinhaService {
     }
 
     public void salvar(Cozinha cozinha) {
-        cozinhaRepository.salvar(cozinha);
+        try {
+            cozinhaRepository.salvar(cozinha);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new EntidadeNaoEncontradaException(cozinha.getId());
+        }
+    }
+
+    public void atualizar(Long id, Cozinha cozinha) {
+        Cozinha cozinhaAtual = buscar(id);
+
+        if (Objects.isNull(cozinhaAtual)) {
+            throw new EntidadeNaoEncontradaException(id);
+        }
+
+        BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+
+        salvar(cozinhaAtual);
     }
 
     public void remover(Long id) {
         try {
             cozinhaRepository.remover(id);
         } catch (EmptyResultDataAccessException exception) {
-            throw new EntidadeNaoEncontradaException(id);
+
         } catch (DataIntegrityViolationException exception) {
             throw new EntidadeEmUsoException(id);
         }
