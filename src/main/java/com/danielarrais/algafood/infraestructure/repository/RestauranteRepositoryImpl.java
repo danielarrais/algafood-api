8815e39b30683601cb091d12d5cdc362449854b3
Daniel Arrais <daniel.carvalho@a.catolica-to.edu.br>
2020-12-12 15:42:43 -0300

    Trata inexistência de um restaurante ao tentar excluir um

--- src/main/java/com/danielarrais/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/danielarrais/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -2,12 +2,14 @@ package com.danielarrais.algafood.infraestructure.repository;
 
 import com.danielarrais.algafood.domain.model.Restaurante;
 import com.danielarrais.algafood.domain.repository.RestauranteRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class RestauranteRepositoryImpl implements RestauranteRepository {
@@ -30,7 +32,13 @@ public class RestauranteRepositoryImpl implements RestauranteRepository {
 
     @Transactional
     public void remover(Long id) {
-        var restaurante = buscar(id);
+        Restaurante restaurante = Optional
+                .ofNullable(buscar(id))
+                .orElseThrow(() -> {
+                    // Esperava a existência de 1 cozinha
+                    throw new EmptyResultDataAccessException(1);
+                });
+
         entityManager.remove(restaurante);
     }
 }
