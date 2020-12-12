84ba9933de84cd1a18dbee5286d7e368aedbdaa2
Daniel Arrais <daniel.carvalho@a.catolica-to.edu.br>
2020-12-12 15:02:35 -0300

    Trata exceções na remoção de uma cozinha

--- src/main/java/com/danielarrais/algafood/domain/service/CozinhaService.java
+++ src/main/java/com/danielarrais/algafood/domain/service/CozinhaService.java
@@ -1,7 +1,11 @@
 package com.danielarrais.algafood.domain.service;
 
+import com.danielarrais.algafood.domain.exception.EntidadeEmUsoException;
+import com.danielarrais.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.danielarrais.algafood.domain.model.Cozinha;
 import com.danielarrais.algafood.domain.repository.CozinhaRepository;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,7 +30,13 @@ public class CozinhaService {
         cozinhaRepository.salvar(cozinha);
     }
 
-    public void remover(Cozinha cozinha) {
-        cozinhaRepository.remover(cozinha);
+    public void remover(Long id) {
+        try {
+            cozinhaRepository.remover(id);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new EntidadeNaoEncontradaException(id);
+        } catch (DataIntegrityViolationException exception) {
+            throw new EntidadeEmUsoException(id);
+        }
     }
 }
