bcb47b748fac3ef99c875e3b462124ac998c5892
Daniel Arrais <daniel.carvalho@a.catolica-to.edu.br>
2020-12-14 20:48:08 -0300

    Cria atributo na exception para diferenciar o tipo de Entidade Nao Encontrada

--- src/main/java/com/danielarrais/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/danielarrais/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,12 +1,23 @@
 package com.danielarrais.algafood.domain.exception;
 
 public class EntidadeNaoEncontradaException extends RuntimeException {
+    private Boolean dependencia = false;
+
     public EntidadeNaoEncontradaException(Long idRegistro) {
         super(String.format("Registro de código %d não existe", idRegistro));
     }
 
     public EntidadeNaoEncontradaException(String entidade, Long idRegistro) {
+        this(entidade, idRegistro, false);
+    }
+
+    public EntidadeNaoEncontradaException(String entidade, Long idRegistro, Boolean dependencia) {
         super(String.format("%s de código %d não existe", entidade, idRegistro));
+        this.dependencia = dependencia;
+    }
+
+    public Boolean isDependencia() {
+        return dependencia;
     }
 }
 
