2829394b1f00864d0bee75029f5b8a47b0c8cb9a
Daniel Arrais <daniel.carvalho@a.catolica-to.edu.br>
2020-12-14 21:00:23 -0300

    Refatora método de alteração de valores de um objeto com base em um MAP

--- src/main/java/com/danielarrais/algafood/util/CustomBeansUtils.java
+++ src/main/java/com/danielarrais/algafood/util/CustomBeansUtils.java
@@ -9,17 +9,16 @@ import java.util.Map;
 import java.util.Objects;
 
 public class CustomBeansUtils extends BeanUtils {
-    public static<T> void mergeValues(Map<String, Object> dadosOrigem, T destino) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            var restauranteOrigem = objectMapper.convertValue(dadosOrigem, destino.getClass());
+    public static <T> void mergeValues(Map<String, Object> dadosOrigem, T destino) {
+        var origemMapper = new ObjectMapper().convertValue(dadosOrigem, destino.getClass());
 
-            dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-                Field field = ReflectionUtils.findField(destino.getClass(), nomePropriedade);
-                Objects.requireNonNull(field).setAccessible(true);
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            Field field = ReflectionUtils.findField(destino.getClass(), nomePropriedade);
+            Objects.requireNonNull(field).setAccessible(true);
 
-                Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+            Object novoValor = ReflectionUtils.getField(field, origemMapper);
 
-                ReflectionUtils.setField(field, destino, novoValor);
-            });
+            ReflectionUtils.setField(field, destino, novoValor);
+        });
     }
 }
