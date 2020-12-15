2ed0c1bbee564aa7c03e6d6740836e41d446da26
Daniel Arrais <daniel.carvalho@a.catolica-to.edu.br>
2020-12-14 23:31:58 -0300

    Atualiza template de services

--- .idea/fileTemplates/Basic Service.java
+++ .idea/fileTemplates/Basic Service.java
@@ -1,17 +1,18 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #set($MODEL_NAME_CAMEL_CASE = $MODEL_NAME.substring(0, 1).toLowerCase() + $MODEL_NAME.substring(1))
 
-import com.danielarrais.algafood.domain.exception.EntidadeEmUsoException;
 import com.danielarrais.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.danielarrais.algafood.domain.model.${MODEL_NAME};
 import com.danielarrais.algafood.domain.repository.${MODEL_NAME}Repository;
+import com.danielarrais.algafood.util.CustomBeansUtils;
+import lombok.SneakyThrows;
 import org.springframework.beans.BeanUtils;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 #parse("File Header.java")
@@ -23,52 +24,41 @@ public class ${MODEL_NAME}Service {
     }
 
     public List<${MODEL_NAME}> listar() {
-        return ${MODEL_NAME_CAMEL_CASE}Repository.listar();
+        return ${MODEL_NAME_CAMEL_CASE}Repository.findAll();
     }
 
-    public ${MODEL_NAME} buscar(long ${MODEL_NAME_CAMEL_CASE}Id) {
-        return ${MODEL_NAME_CAMEL_CASE}Repository.buscar(${MODEL_NAME_CAMEL_CASE}Id);
+    public Optional<${MODEL_NAME}> buscar(long ${MODEL_NAME_CAMEL_CASE}Id) {
+        return ${MODEL_NAME_CAMEL_CASE}Repository.findById(${MODEL_NAME_CAMEL_CASE}Id);
     }
 
+    @SneakyThrows
     public void salvar(${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}) {
-        try {
-            ${MODEL_NAME_CAMEL_CASE}Repository.salvar(${MODEL_NAME_CAMEL_CASE});
-        } catch (EmptyResultDataAccessException exception) {
-            throw new EntidadeNaoEncontradaException(${MODEL_NAME_CAMEL_CASE}.getId());
-        }
+        ${MODEL_NAME_CAMEL_CASE}Repository.save(${MODEL_NAME_CAMEL_CASE});
     }
 
     public void atualizar(Long id, ${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}) {
-        ${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}Atual = buscar(id);
-
-        if (Objects.isNull(${MODEL_NAME_CAMEL_CASE}Atual)) {
+        buscar(id).map(${MODEL_NAME_CAMEL_CASE}Atual -> {
+            BeanUtils.copyProperties(${MODEL_NAME_CAMEL_CASE}, ${MODEL_NAME_CAMEL_CASE}Atual, "id");
+            return ${MODEL_NAME_CAMEL_CASE}Repository.save(${MODEL_NAME_CAMEL_CASE}Atual);
+        }).orElseThrow(() -> {
             throw new EntidadeNaoEncontradaException(id);
-        }
-
-        BeanUtils.copyProperties(${MODEL_NAME_CAMEL_CASE}, ${MODEL_NAME_CAMEL_CASE}Atual, "id");
-
-        salvar(${MODEL_NAME_CAMEL_CASE}Atual);
+        });
     }
-    
+
     public void atualizar(Long id, Map<String, Object> propertiesAndValues) {
-        ${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}Atual = buscar(id);
-
-        if (Objects.isNull(${MODEL_NAME_CAMEL_CASE}Atual)) {
+        buscar(id).map(${MODEL_NAME_CAMEL_CASE}Atual -> {
+            CustomBeansUtils.mergeValues(propertiesAndValues, ${MODEL_NAME_CAMEL_CASE}Atual);
+            return ${MODEL_NAME_CAMEL_CASE}Repository.save(${MODEL_NAME_CAMEL_CASE}Atual);
+        }).orElseThrow(() -> {
             throw new EntidadeNaoEncontradaException(id);
-        }
-
-        CustomBeansUtils.mergeValues(propertiesAndValues, ${MODEL_NAME_CAMEL_CASE}Atual);
-
-        salvar(${MODEL_NAME_CAMEL_CASE}Atual);
+        });
     }
 
     public void remover(Long id) {
         try {
-            ${MODEL_NAME_CAMEL_CASE}Repository.remover(id);
+            ${MODEL_NAME_CAMEL_CASE}Repository.deleteById(id);
         } catch (EmptyResultDataAccessException exception) {
             throw new EntidadeNaoEncontradaException(id);
-        } catch (DataIntegrityViolationException exception) {
-            throw new EntidadeEmUsoException(id);
         }
     }
 }
