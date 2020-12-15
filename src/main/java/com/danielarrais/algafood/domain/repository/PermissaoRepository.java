1a0982c01798679a9be71e6a3b32d7661f0fc0ab
Daniel Arrais <daniel.carvalho@a.catolica-to.edu.br>
2020-12-14 23:30:13 -0300

    Refatora repositórios para herdarem a interface de repositório do JPA

--- src/main/java/com/danielarrais/algafood/domain/repository/PermissaoRepository.java
+++ src/main/java/com/danielarrais/algafood/domain/repository/PermissaoRepository.java
@@ -1,12 +1,9 @@
 package com.danielarrais.algafood.domain.repository;
 
 import com.danielarrais.algafood.domain.model.Permissao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface PermissaoRepository {
-    List<Permissao> listar();
-    Permissao buscar(Long id);
-    void salvar(Permissao permissao);
-    void remover(Long id);
+@Repository
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
 }
--- src/main/java/com/danielarrais/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/danielarrais/algafood/domain/repository/RestauranteRepository.java
@@ -1,12 +1,9 @@
 package com.danielarrais.algafood.domain.repository;
 
 import com.danielarrais.algafood.domain.model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface RestauranteRepository {
-    List<Restaurante> listar();
-    Restaurante buscar(Long id);
-    void salvar(Restaurante Restaurante);
-    void remover(Long id);
+@Repository
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 }
