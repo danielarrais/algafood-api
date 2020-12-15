e49468508020f8bf133fff0ab7c933fc4e9a5929
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-12-14 22:46:54 -0300

    Atualiza template de implementação de repositório

--- .idea/fileTemplates/Basic Repository Implementation.java
+++ .idea/fileTemplates/Basic Repository Implementation.java
@@ -1,38 +1,46 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #set($MODEL_NAME_CAMEL_CASE = $MODEL_NAME.substring(0, 1).toLowerCase() + $MODEL_NAME.substring(1))
 
-import com.danielarrais.algafood.domain.model. ${MODEL_NAME};
-import com.danielarrais.algafood.domain.repository. ${MODEL_NAME}Repository;
-import org.springframework.stereotype.Component;
+import com.danielarrais.algafood.domain.model.${MODEL_NAME};
+import com.danielarrais.algafood.domain.repository.${MODEL_NAME}Repository;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
-@Component
+@Repository
 #parse("File Header.java")
 public class  ${MODEL_NAME}RepositoryImpl implements  ${MODEL_NAME}Repository {
 
     @PersistenceContext
     private EntityManager entityManager;
 
-    public List<${MODEL_NAME}> all() {
+    public List<${MODEL_NAME}> listar() {
         return entityManager.createQuery("select c from ${MODEL_NAME} as c", ${MODEL_NAME}.class).getResultList();
     }
 
     @Transactional
-    public ${MODEL_NAME} add(${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}) {
-        return entityManager.merge(${MODEL_NAME_CAMEL_CASE});
+    public void salvar(${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}) {
+        entityManager.merge(${MODEL_NAME_CAMEL_CASE});
     }
 
-    public ${MODEL_NAME} find(Long id) {
+    public ${MODEL_NAME} buscar(Long id) {
         return entityManager.find(${MODEL_NAME}.class, id);
     }
 
     @Transactional
-    public void remove(${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE}) {
-        ${MODEL_NAME_CAMEL_CASE} = find(${MODEL_NAME_CAMEL_CASE}.getId());
+    public void remover(Long id) {
+        ${MODEL_NAME} ${MODEL_NAME_CAMEL_CASE} = Optional
+                .ofNullable(buscar(id))
+                .orElseThrow(() -> {
+                    // Esperava a existência de 1 cozinha
+                    throw new EmptyResultDataAccessException(1);
+                });
+
         entityManager.remove(${MODEL_NAME_CAMEL_CASE});
     }
 }
