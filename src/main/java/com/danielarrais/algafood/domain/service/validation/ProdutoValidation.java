c73053d2470c8b2784e2562b62445dae9de3ff15
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-20 20:09:01 -0300

    Cria classe de validação para produto

---
+++ src/main/java/com/danielarrais/algafood/domain/service/validation/ProdutoValidation.java
@@ -0,0 +1,24 @@
+package com.danielarrais.algafood.domain.service.validation;
+
+import com.danielarrais.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.danielarrais.algafood.domain.model.Produto;
+import com.danielarrais.algafood.domain.repository.RestauranteRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoValidation {
+    private final RestauranteRepository restauranteRepository;
+
+    public ProdutoValidation(RestauranteRepository restauranteRepository) {
+        this.restauranteRepository = restauranteRepository;
+    }
+
+    public void validateExistenceRestaurante(Produto restaurante) {
+        Long restauranteId = restaurante.getRestaurante().getId();
+        boolean existsRestaurante = restauranteRepository.existsById(restauranteId);
+
+        if (!existsRestaurante) {
+            throw new EntidadeNaoEncontradaException("Restaurante", restauranteId, true);
+        }
+    }
+}
