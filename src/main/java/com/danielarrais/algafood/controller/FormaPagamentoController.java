ff173623eaf13d30fc10f9ec921401b335f3ec2a
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-20 18:44:08 -0300

    Cria controller para Formas de Pagamento

---
+++ src/main/java/com/danielarrais/algafood/controller/FormaPagamentoController.java
@@ -0,0 +1,78 @@
+package com.danielarrais.algafood.controller;
+
+import com.danielarrais.algafood.domain.exception.EntidadeEmUsoException;
+import com.danielarrais.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.danielarrais.algafood.domain.model.FormaPagamento;
+import com.danielarrais.algafood.domain.service.FormaPagamentoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/formas-pagamento")
+public class FormaPagamentoController {
+    private final FormaPagamentoService formaPagamentoService;
+
+    public FormaPagamentoController(FormaPagamentoService formaPagamentoService) {
+        this.formaPagamentoService = formaPagamentoService;
+    }
+
+    @GetMapping()
+    public List<FormaPagamento> listar() {
+        return formaPagamentoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FormaPagamento> buscar(@PathVariable Long id) {
+        return formaPagamentoService.buscar(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void adicionar(@RequestBody FormaPagamento formaPagamento) {
+        formaPagamentoService.salvar(formaPagamento);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody FormaPagamento formaPagamento) {
+        try {
+            formaPagamentoService.atualizar(id, formaPagamento);
+            return ResponseEntity.noContent().build();
+        } catch (EntidadeNaoEncontradaException exception) {
+            return exception.isDependencia() ?
+                    ResponseEntity.badRequest().body(exception.getMessage()) :
+                    ResponseEntity.notFound().build();
+        }
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> valores) {
+        try {
+            formaPagamentoService.atualizar(id, valores);
+            return ResponseEntity.noContent().build();
+        } catch (EntidadeNaoEncontradaException exception) {
+            return exception.isDependencia() ?
+                    ResponseEntity.badRequest().body(exception.getMessage()) :
+                    ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        try {
+            formaPagamentoService.remover(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntidadeEmUsoException exception) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        } catch (EntidadeNaoEncontradaException exception) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
