599ec99fc6af9346bf76422c5605a751bbb4fc0e
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-20 17:04:17 -0300

    Estende repositório customizado de simple repository para remove injeção desnecessária

--- src/main/java/com/danielarrais/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/danielarrais/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -1,23 +1,25 @@
 package com.danielarrais.algafood.infraestructure.repository;
 
 import com.danielarrais.algafood.domain.model.Restaurante;
-import com.danielarrais.algafood.domain.repository.RestauranteRepository;
-import com.danielarrais.algafood.domain.repository.custom.RestauranteRepositoryCustom;
+import com.danielarrais.algafood.domain.repository.custom.RestauranteRepositoryCustomQueries;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 import static com.danielarrais.algafood.infraestructure.spec.RestauranteSpecs.comFreteGratis;
 
 @Repository
-public class RestauranteRepositoryImpl implements RestauranteRepositoryCustom {
-    @Lazy
+public class RestauranteRepositoryImpl extends SimpleJpaRepository<Restaurante, Long> implements RestauranteRepositoryCustomQueries {
+
     @Autowired
-    private RestauranteRepository repository;
+    public RestauranteRepositoryImpl(EntityManager entityManager) {
+        super(Restaurante.class, entityManager);
+    }
 
     public List<Restaurante> findComFreteGratis() {
-        return repository.findAll(comFreteGratis());
+        return findAll(comFreteGratis());
     }
 }
