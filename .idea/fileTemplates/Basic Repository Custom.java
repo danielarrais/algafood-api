324071871f542ad1880498398b84dd618ac76463
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-20 18:22:00 -0300

    Atualiza templates de repository e service

--- .idea/fileTemplates/Basic Repository Custom.java
+++ .idea/fileTemplates/Basic Repository Custom.java
@@ -1,7 +1,7 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#set($MODEL_NAME_CAMEL_CASE = $MODEL_NAME.substring(0, 1).toLowerCase() + $MODEL_NAME.substring(1))
+
+import com.danielarrais.algafood.domain.model.${MODEL_NAME};
 
 #parse("File Header.java")
-public interface ${MODEL_NAME}RepositoryCustom {
+public interface ${MODEL_NAME}RepositoryCustomQueries {
 }
-
