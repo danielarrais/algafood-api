d5bf9c7afc7cf09c8f45f1e75a63d218294861bf
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-25 03:04:25 -0300

    Trata exceções ao excluir cozinha

--- src/main/java/com/danielarrais/algafood/domain/service/CozinhaService.java
+++ src/main/java/com/danielarrais/algafood/domain/service/CozinhaService.java
@@ -1,11 +1,14 @@
 package com.danielarrais.algafood.domain.service;
 
+import com.danielarrais.algafood.domain.exception.EntidadeEmUsoException;
 import com.danielarrais.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.danielarrais.algafood.domain.model.Cozinha;
 import com.danielarrais.algafood.domain.repository.CozinhaRepository;
 import com.danielarrais.algafood.util.CustomBeansUtils;
 import lombok.SneakyThrows;
 import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -52,6 +55,12 @@ public class CozinhaService {
     }
 
     public void remover(Long id) {
-        cozinhaRepository.deleteById(id);
+        try {
+            cozinhaRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new EntidadeNaoEncontradaException(id);
+        } catch (DataIntegrityViolationException exception) {
+            throw new EntidadeEmUsoException(id);
+        }
     }
 }
