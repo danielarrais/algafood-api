4c9d05abd8d765696f5656e635da35896aaa8cdf
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-25 02:44:42 -0300

    Fixa HTTP status para as exceções criadas

--- src/main/java/com/danielarrais/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/danielarrais/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,5 +1,9 @@
 package com.danielarrais.algafood.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntidadeNaoEncontradaException extends RuntimeException {
     private Boolean dependencia = false;
 
