c1a13367bc4b42593d2abf0afcfed5b2ad8c7658
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-26 17:58:29 -0300

    Refatora os handler para atender a  RFC 7807

--- src/main/java/com/danielarrais/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/danielarrais/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -11,52 +11,53 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RegistroNaoEncontradoException.class)
-    public ResponseEntity<?> tratarRegistroNaoEncontradoException(RegistroNaoEncontradoException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
+    public ResponseEntity<?> handleRegistroNaoEncontradoException(RegistroNaoEncontradoException e, WebRequest request) {
+        Problem problem = Problem.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .title("Registro não encontrado")
+                .detail(e.getMessage())
                 .build();
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+        return super.handleExceptionInternal(e, problem, new HttpHeaders(), problem.getHttpStatus(), request);
     }
 
     @ExceptionHandler(DependenciaNaoEncontradaException.class)
-    public ResponseEntity<?> tratarDependenciaNaoEncontradaException(DependenciaNaoEncontradaException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
+    public ResponseEntity<?> handleDependenciaNaoEncontradaException(DependenciaNaoEncontradaException e, WebRequest request) {
+        Problem problem = Problem.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .title("Dependência não encontrada")
+                .detail(e.getMessage())
                 .build();
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+        return super.handleExceptionInternal(e, problem, new HttpHeaders(), problem.getHttpStatus(), request);
     }
 
     @ExceptionHandler(RegistroEmUsoException.class)
-    public ResponseEntity<?> tratarRegistroEmUsoException(RegistroEmUsoException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
+    public ResponseEntity<?> handleRegistroEmUsoException(RegistroEmUsoException e, WebRequest request) {
+        Problem problem = Problem.builder()
+                .status(HttpStatus.CONFLICT)
+                .title("Registro em uso")
+                .detail(e.getMessage())
                 .build();
 
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+        return super.handleExceptionInternal(e, problem, new HttpHeaders(), problem.getHttpStatus(), request);
     }
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal( Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
         if (body == null) {
-            body = Problema.builder()
-                    .dataHora(LocalDateTime.now())
-                    .mensagem(status.getReasonPhrase())
+            body = Problem.builder()
+                    .status(status)
+                    .title(status.getReasonPhrase())
                     .build();
         } else if (body instanceof String) {
-            body = Problema.builder()
-                    .dataHora(LocalDateTime.now())
-                    .mensagem((String) body)
+            body = Problem.builder()
+                    .status(status)
+                    .title((String) body)
                     .build();
         }
 
