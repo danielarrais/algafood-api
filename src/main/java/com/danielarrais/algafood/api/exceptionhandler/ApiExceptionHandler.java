51408ee6cf4d59f8fcc7df2c7e4d409c92e93c9e
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-26 17:16:36 -0300

    Cria corpo para handlers de exceções padrões

--- src/main/java/com/danielarrais/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/danielarrais/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,15 +3,19 @@ package com.danielarrais.algafood.api.exceptionhandler;
 import com.danielarrais.algafood.domain.exception.DependenciaNaoEncontradaException;
 import com.danielarrais.algafood.domain.exception.RegistroEmUsoException;
 import com.danielarrais.algafood.domain.exception.RegistroNaoEncontradoException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(RegistroNaoEncontradoException.class)
     public ResponseEntity<?> tratarRegistroNaoEncontradoException(RegistroNaoEncontradoException e) {
         Problema problema = Problema.builder()
@@ -40,5 +44,22 @@ public class ApiExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal( Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        if (body == null) {
+            body = Problema.builder()
+                    .dataHora(LocalDateTime.now())
+                    .mensagem(status.getReasonPhrase())
+                    .build();
+        } else if (body instanceof String) {
+            body = Problema.builder()
+                    .dataHora(LocalDateTime.now())
+                    .mensagem((String) body)
+                    .build();
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
