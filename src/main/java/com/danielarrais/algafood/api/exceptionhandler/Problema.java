69c97a7aa5feb36ed3be5cd876b12d96d3bd92dc
danielarrais <danielarraiscarvalho@gmail.com>
2020-12-26 17:15:48 -0300

    Refatora classe de representação do problema

--- src/main/java/com/danielarrais/algafood/api/exceptionhandler/Problema.java
+++ src/main/java/com/danielarrais/algafood/api/exceptionhandler/Problema.java
@@ -1,16 +1,12 @@
 package com.danielarrais.algafood.api.exceptionhandler;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
+@Data
 public class Problema {
     private LocalDateTime dataHora;
     private String mensagem;
