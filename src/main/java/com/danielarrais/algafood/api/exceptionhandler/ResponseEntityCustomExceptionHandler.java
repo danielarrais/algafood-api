0d0be6f4e5d59333d56059fcdb51bc97b9d4ab5c
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-10 16:44:27 -0300

    Sobrescreve tratamento de NoHandlerFoundException

--- src/main/java/com/danielarrais/algafood/api/exceptionhandler/ResponseEntityCustomExceptionHandler.java
+++ src/main/java/com/danielarrais/algafood/api/exceptionhandler/ResponseEntityCustomExceptionHandler.java
@@ -7,10 +7,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ResponseEntityCustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String detail = String.format("O recurso %s, que você tentou acessar, é inexistente.", e.getRequestURL());
+
+        Problem problem = Problem.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .title("Recurso não encontrado")
+                .detail(detail)
+                .build();
+
+        return super.handleExceptionInternal(e, problem, headers, problem.getHttpStatus(), request);
+    }
+
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Problem problem = Problem.builder()
