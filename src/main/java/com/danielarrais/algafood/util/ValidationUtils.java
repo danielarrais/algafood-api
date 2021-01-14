cc4a9733f6740d9417d748d6d14c3b8d6367ade2
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-13 21:20:39 -0300

    Injeta ResourceBundleMessageSource no validations útils

--- src/main/java/com/danielarrais/algafood/util/ValidationUtils.java
+++ src/main/java/com/danielarrais/algafood/util/ValidationUtils.java
@@ -2,6 +2,7 @@ package com.danielarrais.algafood.util;
 
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
@@ -9,22 +10,19 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class ValidationUtils {
+    private static ResourceBundleMessageSource messageSource;
+
+    public ValidationUtils(ResourceBundleMessageSource messageSource) {
+        ValidationUtils.messageSource = messageSource;
+    }
+
     public static List<Map<String, String>> extractErrorsFrom(BindingResult bindingResult){
         var validationErrors = new ArrayList<Map<String, String>>();
-        var messageSource = new ResourceBundleMessageSource();
-
-        messageSource.setBasenames("messages");
-        messageSource.setUseCodeAsDefaultMessage(true);
-        messageSource.setDefaultEncoding("UTF-8");
 
         bindingResult.getFieldErrors().forEach(error -> {
-            var objectNameKebabCase = StringUtils.camelToKebab(error.getObjectName());
-
-            messageSource.setBasenames(String.format("messages/%s", objectNameKebabCase));
-
             String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
-
             validationErrors.add(Collections.singletonMap(error.getField(), message));
         });
 
