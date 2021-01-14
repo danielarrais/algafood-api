9a35dd203aae3408f979acfcdc254c10e9819ff2
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-13 21:20:23 -0300

    Cria bean para injetar ResourceBundleMessageSource com as configurações corretas

--- src/main/java/com/danielarrais/algafood/core/validation/ValidationConfig.java
+++ src/main/java/com/danielarrais/algafood/core/validation/ValidationConfig.java
@@ -3,10 +3,21 @@ package com.danielarrais.algafood.core.validation;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Configuration
 public class ValidationConfig {
+    final ResourcePatternResolver resourceResolver;
+
+    public ValidationConfig(ResourcePatternResolver resourceResolver) {
+        this.resourceResolver = resourceResolver;
+    }
 
     @Bean
     public LocalValidatorFactoryBean localValidatorFactoryBean(MessageSource messageSource) {
@@ -14,5 +25,27 @@ public class ValidationConfig {
         localValidatorFactoryBean.setValidationMessageSource(messageSource);
 
         return localValidatorFactoryBean;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource resourceBundleMessageSource() throws IOException {
+        var messageSource = new ResourceBundleMessageSource();
+        var resources = messagesResources();
+
+        messageSource.setBasenames(resources);
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+
+    private String[] messagesResources() throws IOException {
+        return Arrays
+                .stream(resourceResolver.getResources("classpath:messages/*.properties"))
+                .map(resource -> {
+                    var fileName = Objects.requireNonNull(resource.getFilename()).replace(".properties", "");
+                    return String.format("messages/%s", fileName);
+                })
+                .toArray(String[]::new);
     }
 }
