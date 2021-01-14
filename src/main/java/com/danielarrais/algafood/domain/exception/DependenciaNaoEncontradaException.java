b3e0ce954b25dddde12b10920036224266bad110
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-13 23:51:48 -0300

    Remove anotações desnecessárias

--- src/main/java/com/danielarrais/algafood/domain/exception/DependenciaNaoEncontradaException.java
+++ src/main/java/com/danielarrais/algafood/domain/exception/DependenciaNaoEncontradaException.java
@@ -1,9 +1,5 @@
 package com.danielarrais.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DependenciaNaoEncontradaException extends RegistroNaoEncontradoException {
     public DependenciaNaoEncontradaException(String entidade, Long idRegistro) {
         super(String.format("%s de código %d não existe", entidade, idRegistro));
