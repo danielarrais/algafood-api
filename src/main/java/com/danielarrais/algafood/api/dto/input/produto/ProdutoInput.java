f9575bda385801c0612e8943c4b020bd76af1e21
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-22 20:20:02 -0300

    Valida obrigatoriedade do restaurante do produto

--- src/main/java/com/danielarrais/algafood/api/dto/input/produto/ProdutoInput.java
+++ src/main/java/com/danielarrais/algafood/api/dto/input/produto/ProdutoInput.java
@@ -8,6 +8,7 @@ import lombok.Data;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 
@@ -24,5 +25,6 @@ public class ProdutoInput {
     private Boolean ativo;
 
     @Valid
+    @NotNull
     private RestauranteIdInput restaurante;
 }
