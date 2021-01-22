039adc8f093c0cb645aa04ce62b85280863cbc61
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-22 00:07:43 -0300

    Ajusta script de carga do banco

--- src/main/resources/db/seed/afterMigrate.sql
+++ src/main/resources/db/seed/afterMigrate.sql
@@ -13,57 +13,57 @@ truncate table cidade restart identity cascade;
 truncate table estado restart identity cascade;
 
 -- Inserts Estados
-INSERT INTO estado (nome) VALUES ('Maranhão'); -- 1
-INSERT INTO estado (nome) VALUES ('Bahia'); -- 2
+INSERT INTO estado (nome, data_cadastro, data_atualizacao) VALUES ('Maranhão', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 1
+INSERT INTO estado (nome, data_cadastro, data_atualizacao) VALUES ('Bahia', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 2
 
 -- Inserts Cidades
-INSERT INTO cidade (nome, estado_id) VALUES ('Sambaíba', 1); -- 1
-INSERT INTO cidade (nome, estado_id) VALUES ('Balsas', 2); -- 2
+INSERT INTO cidade (nome, estado_id, data_cadastro, data_atualizacao) VALUES ('Sambaíba', 1, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 1
+INSERT INTO cidade (nome, estado_id, data_cadastro, data_atualizacao) VALUES ('Balsas', 2, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 2
 
 -- Inserts Permissoes
-INSERT INTO permissao (nome, descricao) VALUES ('ADMIN', 'Permite tudo'); -- 1
-INSERT INTO permissao (nome, descricao) VALUES ('ALUNO', 'Permite emição de certificados'); -- 2
+INSERT INTO permissao (nome, descricao, data_cadastro, data_atualizacao) VALUES ('ADMIN', 'Permite tudo', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 1
+INSERT INTO permissao (nome, descricao, data_cadastro, data_atualizacao) VALUES ('ALUNO', 'Permite emição de certificados', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 2
 
 -- Inserts Grupos
-INSERT INTO grupo (nome) VALUES ('Alunos'); -- 1
-INSERT INTO grupo (nome) VALUES ('Cordenadores'); -- 2
-INSERT INTO grupo (nome) VALUES ('Terceirizados'); -- 3
-INSERT INTO grupo (nome) VALUES ('Reitoria'); -- 4
+INSERT INTO grupo (nome, data_cadastro, data_atualizacao) VALUES ('Alunos', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 1
+INSERT INTO grupo (nome, data_cadastro, data_atualizacao) VALUES ('Cordenadores', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 2
+INSERT INTO grupo (nome, data_cadastro, data_atualizacao) VALUES ('Terceirizados', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 3
+INSERT INTO grupo (nome, data_cadastro, data_atualizacao) VALUES ('Reitoria', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 4
 
 -- Inserts Usuários
-INSERT INTO usuario (nome, email, senha, data_cadastro) VALUES ('Daniel', 'daniel@email.com', 'Sambaíba', (now() at time zone 'utc')); -- 1
-INSERT INTO usuario (nome, email, senha, data_cadastro) VALUES ('Maria', 'maria@email.com', 'Sambaíba', (now() at time zone 'utc')); -- 2
-INSERT INTO usuario (nome, email, senha, data_cadastro) VALUES ('João', 'joão@email.com', 'Sambaíba', (now() at time zone 'utc')); -- 3
-INSERT INTO usuario (nome, email, senha, data_cadastro) VALUES ('Joana', 'joana@email.com', 'Sambaíba', (now() at time zone 'utc')); -- 4
+INSERT INTO usuario (nome, email, senha, data_cadastro, data_atualizacao) VALUES ('Daniel', 'daniel@email.com', 'Sambaíba', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 1
+INSERT INTO usuario (nome, email, senha, data_cadastro, data_atualizacao) VALUES ('Maria', 'maria@email.com', 'Sambaíba', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 2
+INSERT INTO usuario (nome, email, senha, data_cadastro, data_atualizacao) VALUES ('João', 'joão@email.com', 'Sambaíba', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 3
+INSERT INTO usuario (nome, email, senha, data_cadastro, data_atualizacao) VALUES ('Joana', 'joana@email.com', 'Sambaíba', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 4
 
 -- Inserts Cozinha
-INSERT INTO cozinha (nome) VALUES ('Nordestina'); -- 1
-INSERT INTO cozinha (nome) VALUES ('Francesa'); -- 2
-INSERT INTO cozinha (nome) VALUES ('Italiana'); -- 3
-INSERT INTO cozinha (nome) VALUES ('Islamica'); -- 4
+INSERT INTO cozinha (nome, data_cadastro, data_atualizacao) VALUES ('Nordestina', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 1
+INSERT INTO cozinha (nome, data_cadastro, data_atualizacao) VALUES ('Francesa', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 2
+INSERT INTO cozinha (nome, data_cadastro, data_atualizacao) VALUES ('Italiana', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 3
+INSERT INTO cozinha (nome, data_cadastro, data_atualizacao) VALUES ('Islamica', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 4
 
 -- Inserts Restaurantes
-INSERT INTO restaurante (nome, cozinha_id, taxa_frete, ativo, data_cadastro, data_atualizacao, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade_id) VALUES ('Churrascaria Tradição', 1, 10.00, true, (now() at time zone 'utc'), (now() at time zone 'utc'), '77060140', 'Rua Mato Grosso e Mathias', 'S/N', 'Predio mais alto da cidade', 'Aureny 1', 1);
-INSERT INTO restaurante (nome, cozinha_id, taxa_frete, ativo, data_cadastro, data_atualizacao, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade_id) VALUES ('Jackin Predident', 2, 5.00, true, (now() at time zone 'utc'), (now() at time zone 'utc'), '77060140', 'Rua Mato Grosso e Mathias', 'S/N', 'Predio mais alto da cidade', 'Aureny 1', 2);
-INSERT INTO restaurante (nome, cozinha_id, taxa_frete, ativo, data_cadastro, data_atualizacao, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade_id) VALUES ('Espetinho do Luiz', 3, 0.00, true, (now() at time zone 'utc'), (now() at time zone 'utc'), '77060140', 'Rua Mato Grosso e Mathias', 'S/N', 'Predio mais alto da cidade', 'Aureny 1', 1);
-INSERT INTO restaurante (nome, cozinha_id, taxa_frete, ativo, data_cadastro, data_atualizacao, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade_id) VALUES ('K-ane Lanches', 4, 0.00, true, (now() at time zone 'utc'), (now() at time zone 'utc'), '77060140', 'Rua Mato Grosso e Mathias', 'S/N', 'Predio mais alto da cidade', 'Aureny 1', 2);
+INSERT INTO restaurante (nome, cozinha_id, taxa_frete, ativo, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade_id, data_cadastro, data_atualizacao) VALUES ('Churrascaria Tradição', 1, 10.00, true,  '77060140', 'Rua Mato Grosso e Mathias', 'S/N', 'Predio mais alto da cidade', 'Aureny 1', 1, (now() at time zone 'utc'), (now() at time zone 'utc'));
+INSERT INTO restaurante (nome, cozinha_id, taxa_frete, ativo, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade_id, data_cadastro, data_atualizacao) VALUES ('Jackin Predident', 2, 5.00, true,  '77060140', 'Rua Mato Grosso e Mathias', 'S/N', 'Predio mais alto da cidade', 'Aureny 1', 2, (now() at time zone 'utc'), (now() at time zone 'utc'));
+INSERT INTO restaurante (nome, cozinha_id, taxa_frete, ativo, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade_id, data_cadastro, data_atualizacao) VALUES ('Espetinho do Luiz', 3, 0.00, true,  '77060140', 'Rua Mato Grosso e Mathias', 'S/N', 'Predio mais alto da cidade', 'Aureny 1', 1, (now() at time zone 'utc'), (now() at time zone 'utc'));
+INSERT INTO restaurante (nome, cozinha_id, taxa_frete, ativo, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro, endereco_cidade_id, data_cadastro, data_atualizacao) VALUES ('K-ane Lanches', 4, 0.00, true,  '77060140', 'Rua Mato Grosso e Mathias', 'S/N', 'Predio mais alto da cidade', 'Aureny 1', 2, (now() at time zone 'utc'), (now() at time zone 'utc'));
 
 -- Inserts Formas de Pagamento
-INSERT INTO forma_pagamento (descricao) VALUES ('Dinheiro'); -- 1
-INSERT INTO forma_pagamento (descricao) VALUES ('Cheque'); -- 2
-INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de débito'); -- 3
-INSERT INTO forma_pagamento (descricao) VALUES ('Boleto bancário'); -- 4
-INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de crédito'); -- 5
+INSERT INTO forma_pagamento (descricao, data_cadastro, data_atualizacao) VALUES ('Dinheiro', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 1
+INSERT INTO forma_pagamento (descricao, data_cadastro, data_atualizacao) VALUES ('Cheque', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 2
+INSERT INTO forma_pagamento (descricao, data_cadastro, data_atualizacao) VALUES ('Cartão de débito', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 3
+INSERT INTO forma_pagamento (descricao, data_cadastro, data_atualizacao) VALUES ('Boleto bancário', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 4
+INSERT INTO forma_pagamento (descricao, data_cadastro, data_atualizacao) VALUES ('Cartão de crédito', (now() at time zone 'utc'), (now() at time zone 'utc')); -- 5
 
 -- Inserts Produtos
-INSERT INTO public.produto (descricao, preco, ativo, restaurante_id) VALUES ('Prato feito', 10.00, true, 1); -- 1
-INSERT INTO public.produto (descricao, preco, ativo, restaurante_id) VALUES ('Churrasco', 50.00, true, 2); -- 2
-INSERT INTO public.produto (descricao, preco, ativo, restaurante_id) VALUES ('Espetinho', 15.00, true, 3); -- 3
-INSERT INTO public.produto (descricao, preco, ativo, restaurante_id) VALUES ('Cozido', 18.00, true, 4); -- 4
-INSERT INTO public.produto (descricao, preco, ativo, restaurante_id) VALUES ('Macarrão', 10.00, true, 1); -- 5
-INSERT INTO public.produto (descricao, preco, ativo, restaurante_id) VALUES ('Refrigerante 350ML', 3.50, true, 2); -- 6
-INSERT INTO public.produto (descricao, preco, ativo, restaurante_id) VALUES ('Suco Laranja', 5.00, true, 3); -- 7
-INSERT INTO public.produto (descricao, preco, ativo, restaurante_id) VALUES ('Suco Abacaxi', 8.00, true, 4); -- 8
+INSERT INTO produto (descricao, preco, ativo, restaurante_id, data_cadastro, data_atualizacao) VALUES ('Prato feito', 10.00, true, 1, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 1
+INSERT INTO produto (descricao, preco, ativo, restaurante_id, data_cadastro, data_atualizacao) VALUES ('Churrasco', 50.00, true, 2, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 2
+INSERT INTO produto (descricao, preco, ativo, restaurante_id, data_cadastro, data_atualizacao) VALUES ('Espetinho', 15.00, true, 3, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 3
+INSERT INTO produto (descricao, preco, ativo, restaurante_id, data_cadastro, data_atualizacao) VALUES ('Cozido', 18.00, true, 4, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 4
+INSERT INTO produto (descricao, preco, ativo, restaurante_id, data_cadastro, data_atualizacao) VALUES ('Macarrão', 10.00, true, 1, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 5
+INSERT INTO produto (descricao, preco, ativo, restaurante_id, data_cadastro, data_atualizacao) VALUES ('Refrigerante 350ML', 3.50, true, 2, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 6
+INSERT INTO produto (descricao, preco, ativo, restaurante_id, data_cadastro, data_atualizacao) VALUES ('Suco Laranja', 5.00, true, 3, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 7
+INSERT INTO produto (descricao, preco, ativo, restaurante_id, data_cadastro, data_atualizacao) VALUES ('Suco Abacaxi', 8.00, true, 4, (now() at time zone 'utc'), (now() at time zone 'utc')); -- 8
 
 -- Inserts Formas de Pagamento
 INSERT INTO forma_pagamento_restaurante (restaurante_id, forma_pagamento_id) VALUES (1, 1);
@@ -90,7 +90,7 @@ INSERT INTO grupo_permissao (grupo_id, permissao_id) VALUES (3, 2);
 INSERT INTO grupo_permissao (grupo_id, permissao_id) VALUES (4, 2);
 
 -- Inserts Grupos de Usuário
-INSERT INTO public.usuario_grupo (usuario_id, grupo_id) VALUES (1, 1);
-INSERT INTO public.usuario_grupo (usuario_id, grupo_id) VALUES (2, 2);
-INSERT INTO public.usuario_grupo (usuario_id, grupo_id) VALUES (3, 3);
-INSERT INTO public.usuario_grupo (usuario_id, grupo_id) VALUES (4, 4);
+INSERT INTO usuario_grupo (usuario_id, grupo_id) VALUES (1, 1);
+INSERT INTO usuario_grupo (usuario_id, grupo_id) VALUES (2, 2);
+INSERT INTO usuario_grupo (usuario_id, grupo_id) VALUES (3, 3);
+INSERT INTO usuario_grupo (usuario_id, grupo_id) VALUES (4, 4);
