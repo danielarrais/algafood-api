1f7af11a4fc2cd2915887f3e49684017bb8d9880
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-25 20:11:44 -0300

    Adiciona novo construtor para receber a causa da exceção

--- src/main/java/com/danielarrais/algafood/domain/exception/NegocioException.java
+++ src/main/java/com/danielarrais/algafood/domain/exception/NegocioException.java
@@ -4,5 +4,9 @@ public class NegocioException extends RuntimeException {
     public NegocioException(String message) {
         super(message);
     }
+
+    public NegocioException(String message, Exception e) {
+        super(message, e);
+    }
 }
 
