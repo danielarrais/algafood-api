973a11eed51e73ec5349ea2bfbad9f87363718f4
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-25 17:01:04 -0300

    Cria controller com end points para controle de formas de pagamento de restaurante

---
+++ src/main/java/com/danielarrais/algafood/api/controller/RestauranteFormasPagamentoController.java
@@ -0,0 +1,38 @@
+package com.danielarrais.algafood.api.controller;
+
+import com.danielarrais.algafood.api.dto.output.formaPagamento.FormaPagamentoOutput;
+import com.danielarrais.algafood.domain.service.RestauranteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.danielarrais.algafood.util.ModelMapperUtils.mapper;
+
+@RestController
+@RequestMapping("/restaurantes/{idRestaurante}/formas-pagamento")
+public class RestauranteFormasPagamentoController {
+    private final RestauranteService restauranteService;
+
+    public RestauranteFormasPagamentoController(RestauranteService restauranteService) {
+        this.restauranteService = restauranteService;
+    }
+
+    @GetMapping()
+    public List<FormaPagamentoOutput> listarFormasPagamento(@PathVariable Long idRestaurante) {
+        var restaurante = restauranteService.buscarObrigatorio(idRestaurante);
+        return mapper(restaurante.getFormasPagamento(), FormaPagamentoOutput.class);
+    }
+
+    @PutMapping("/{idFormaPagamento}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void adicionarFormaPagamento(@PathVariable Long idRestaurante, @PathVariable Long idFormaPagamento) {
+        restauranteService.adicionarFormaPagamento(idRestaurante, idFormaPagamento);
+    }
+
+    @DeleteMapping("/{idFormaPagamento}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerFormaPagamento(@PathVariable Long idRestaurante, @PathVariable Long idFormaPagamento) {
+        restauranteService.removerFormaPagamento(idRestaurante, idFormaPagamento);
+    }
+}
