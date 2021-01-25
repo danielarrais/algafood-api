13a004a2eb26ff4caad2947e94e8b291765e2572
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-25 15:56:48 -0300

    Cria exceção para violação de regras de negócio

---
+++ src/main/java/com/danielarrais/algafood/domain/exception/NegocioException.java
@@ -0,0 +1,8 @@
+package com.danielarrais.algafood.domain.exception;
+
+public class NegocioException extends RuntimeException {
+    public NegocioException(String message) {
+        super(message);
+    }
+}
+
