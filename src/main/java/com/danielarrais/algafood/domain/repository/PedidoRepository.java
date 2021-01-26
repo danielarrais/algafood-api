27bde00e1fe9606a4fe0fe0111ecc55838203c71
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-25 23:38:59 -0300

    Customiza consulta de pedidos

--- src/main/java/com/danielarrais/algafood/domain/repository/PedidoRepository.java
+++ src/main/java/com/danielarrais/algafood/domain/repository/PedidoRepository.java
@@ -2,7 +2,17 @@ package com.danielarrais.algafood.domain.repository;
 
 import com.danielarrais.algafood.domain.model.Pedido;
 import com.danielarrais.algafood.domain.repository.custom.BaseSimpleJpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PedidoRepository extends BaseSimpleJpaRepository<Pedido, Long> {
+    @Query("from Pedido p " +
+            "join fetch p.cliente " +
+            "join fetch p.restaurante r " +
+            "join fetch r.cozinha " +
+            "join fetch r.endereco.cidade c " +
+            "join fetch c.estado e")
+    List<Pedido> findAll();
 }
 
