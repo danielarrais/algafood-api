77735eaf270a57ad4a0d867e86e5d5e3b4ee5491
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-25 22:58:13 -0300

    Cria repositório para a entidade pedido

---
+++ src/main/java/com/danielarrais/algafood/domain/repository/PedidoRepository.java
@@ -0,0 +1,8 @@
+package com.danielarrais.algafood.domain.repository;
+
+import com.danielarrais.algafood.domain.model.Pedido;
+import com.danielarrais.algafood.domain.repository.custom.BaseSimpleJpaRepository;
+
+public interface PedidoRepository extends BaseSimpleJpaRepository<Pedido, Long> {
+}
+
