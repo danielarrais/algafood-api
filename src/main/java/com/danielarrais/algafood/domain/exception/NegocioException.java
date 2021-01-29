5507da78617e67be75f02c0415fdea2f6e527da1
danielarrais <danielarraiscarvalho@gmail.com>
2021-01-28 23:36:24 -0300

    Cria contrustores que aceitam argumentos para formatar msg

--- src/main/java/com/danielarrais/algafood/domain/exception/NegocioException.java
+++ src/main/java/com/danielarrais/algafood/domain/exception/NegocioException.java
@@ -8,5 +8,9 @@ public class NegocioException extends RuntimeException {
     public NegocioException(String message, Exception e) {
         super(message, e);
     }
+
+    public NegocioException(String erro, Object... args) {
+        super(String.format(erro, args));
+    }
 }
 
--- src/main/java/com/danielarrais/algafood/domain/exception/RegistroNaoEncontradoException.java
+++ src/main/java/com/danielarrais/algafood/domain/exception/RegistroNaoEncontradoException.java
@@ -1,12 +1,17 @@
 package com.danielarrais.algafood.domain.exception;
 
 public class RegistroNaoEncontradoException extends RuntimeException {
-    public RegistroNaoEncontradoException(String nomeEntidade, Long idRegistro) {
-        super(String.format("%s de código %d não existe", nomeEntidade, idRegistro));
-    }
 
     public RegistroNaoEncontradoException(String erro) {
         super(erro);
+    }
+
+    public RegistroNaoEncontradoException(String erro, Object... args) {
+        super(String.format(erro, args));
+    }
+
+    public RegistroNaoEncontradoException(String nomeEntidade, Long idRegistro) {
+        super(String.format("%s de código %d não existe", nomeEntidade, idRegistro));
     }
 }
 
