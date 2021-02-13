f37624cf59c8c4771048426881e7928b22d4e6f8
danielarrais <danielarraiscarvalho@gmail.com>
2021-02-12 22:13:36 -0300

    Cria classe com propriedades de envio de email

---
+++ src/main/java/com/danielarrais/algafood/core/email/EmailProperties.java
@@ -0,0 +1,18 @@
+package com.danielarrais.algafood.core.email;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Component
+@Validated
+@ConfigurationProperties("algafood.email")
+public class EmailProperties {
+
+    @NotEmpty
+    private String remetente;
+}
