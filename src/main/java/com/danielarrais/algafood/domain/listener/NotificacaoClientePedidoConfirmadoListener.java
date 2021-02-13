77903615c19b40add413e5f1c5eb6a5af3f41403
danielarrais <danielarraiscarvalho@gmail.com>
2021-02-13 01:07:40 -0300

    Faz com que e-mail seja enviado após o update

--- src/main/java/com/danielarrais/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/danielarrais/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -3,11 +3,15 @@ package com.danielarrais.algafood.domain.listener;
 import com.danielarrais.algafood.domain.event.PedidoConfirmadoEvent;
 import com.danielarrais.algafood.domain.service.EnvioEmailService;
 import com.danielarrais.algafood.domain.service.EnvioEmailService.Mensagem;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import static com.danielarrais.algafood.domain.service.EnvioEmailService.TemplateEmail.PEDIDO_CONFIRMADO;
+import static org.springframework.transaction.event.TransactionPhase.BEFORE_COMMIT;
 
+@Slf4j
 @Component
 public class NotificacaoClientePedidoConfirmadoListener {
     private final EnvioEmailService envioEmailService;
@@ -17,6 +21,7 @@ public class NotificacaoClientePedidoConfirmadoListener {
     }
 
     @EventListener
+    @TransactionalEventListener(phase = BEFORE_COMMIT)
     public void aoConfirmarPedido(final PedidoConfirmadoEvent event) {
         var pedido = event.getPedido();
         var mensagem = Mensagem.builder()
@@ -25,6 +30,7 @@ public class NotificacaoClientePedidoConfirmadoListener {
                 .outroDado("pedido", pedido)
                 .destinatario(pedido.getCliente().getEmail()).build();
 
+        log.info("Enviou");
         envioEmailService.enviar(mensagem);
     }
 }
