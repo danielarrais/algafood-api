55f38e1376ba0d33634f2d207dff80a2a65d7016
danielarrais <danielarraiscarvalho@gmail.com>
2021-02-18 23:31:10 -0300

    Configura o spring security

---
+++ src/main/java/com/danielarrais/algafood/core/security/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.danielarrais.algafood.core.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and().authorizeRequests()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+    }
+
+}
